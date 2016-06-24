d1c5f3114e0fc3dc4ef66c0da38cb3879aed2105
Raghav RV <rvraghav93@gmail.com>
2016-06-24 16:53:32 +0200

    Use plain IPython image. Have to figure out where I screwed up mpl

--- tree_methods_missing_val_support/get_graph.py
+++ tree_methods_missing_val_support/get_graph.py
@@ -14,29 +14,21 @@ import numpy as np
 
 def get_graph(dtc, feat_names=None, size=[7, 7], max_depth=10):
     n_classes = dtc.n_classes_
-    dot_file = StringIO()
-    image_file = BytesIO()
 
     # Get the dot graph of our decision tree
-    export_graphviz(dtc, out_file=dot_file, feature_names=feat_names,
+    export_graphviz(dtc, out_file="_dotfile.dot", feature_names=feat_names,
                     rounded=True, filled=True,
-                    special_characters=True,
+                    special_characters=False,
                     show_missing_dir=True,
                     class_names=map(str, range(1, n_classes+1)),
                     max_depth=max_depth)
-    dot_file.seek(0)
-
+   
     # Convert this dot graph into an image
-    g = pygraphviz.AGraph(dot_file.read())
+    g = pygraphviz.AGraph("_dotfile.dot")
     g.layout('dot')
-    # g.draw doesn't work when the image object doesn't have a name (with a proper extension)
-    image_file.name = "image.png"
-    image_file.seek(0)
-    g.draw(path=image_file)
-    image_file.seek(0)
+    g.draw(path="_image.png")
 
     # Plot it
-    plt.figure().set_size_inches(*size)
-    plt.axis('off')
-    plt.imshow(img.imread(fname=image_file))
-    plt.show()
+
+    from IPython.display import Image
+    return Image("_image.png")
