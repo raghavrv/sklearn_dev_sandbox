ab7c697c2ce205bc39a550bf23ca54f04924a1ff
Raghav RV <rvraghav93@gmail.com>
2016-08-01 16:49:54 +0200

    gbcv update benchmark script

---
+++ gradient_boosting_trees_with_cross_val/bench_gbcv_threading_vs_multiproc.py
@@ -0,0 +1,16 @@
+from sklearn.ensemble import GradientBoostingRegressorCV, GradientBoostingClassifierCV
+from sklearn.ensemble import GradientBoostingRegressor, GradientBoostingClassifier
+
+from sklearn.datasets import load_boston, fetch_covtype, load_iris, make_classification
+from sklearn.datasets import fetch_20newsgroups_vectorized, fetch_california_housing
+
+from sklearn.model_selection import train_test_split
+
+from time import time
+
+FILENAME = "bench_results_time"
+
+
+
+
+
