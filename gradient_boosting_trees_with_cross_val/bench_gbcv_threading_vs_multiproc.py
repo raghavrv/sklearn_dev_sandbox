365c3add6be47dc197b7758e02568870d57b9e75
Raghav RV <rvraghav93@gmail.com>
2016-08-05 14:46:09 +0200

    Benchmark threading vs multiprocessing

--- gradient_boosting_trees_with_cross_val/bench_gbcv_threading_vs_multiproc.py
+++ gradient_boosting_trees_with_cross_val/bench_gbcv_threading_vs_multiproc.py
@@ -1,16 +1,14 @@
+import numpy as np
+
 from sklearn.ensemble import GradientBoostingRegressorCV, GradientBoostingClassifierCV
 from sklearn.ensemble import GradientBoostingRegressor, GradientBoostingClassifier
 
 from sklearn.datasets import load_boston, fetch_covtype, load_iris, make_classification
 from sklearn.datasets import fetch_20newsgroups_vectorized, fetch_california_housing
 
-from sklearn.model_selection import train_test_split
+gbccv = GradientBoostingClassifierCV(n_jobs=8, random_state=42)
 
-from time import time
+covtype = fetch_covtype()
 
-FILENAME = "bench_results_time"
-
-
-
-
-
+X, y = covtype.data[::2], covtype.target[::2]
+gbccv.fit(X, y)
